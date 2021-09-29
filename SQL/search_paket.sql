37649bbcbec6fe61636d58b4989decdc8d08dcdc
Maulana Reza Pahlevi <79183291+mrezap@users.noreply.github.com>
2021-09-29 11:15:22 +0700

    Modify the indentation

--- SQL/search_paket.sql
+++ SQL/search_paket.sql
@@ -8,17 +8,16 @@ SELECT
 	panitia.pnt_nama
 	--lls_status, lls_penawaran_ulang, lls_evaluasi_ulang
 FROM
-	PUBLIC .paket
+	PUBLIC.paket
 JOIN
-	PUBLIC. ukpbj
-ON paket.ukpbj_id = ukpbj.ukpbj_id
+	PUBLIC.ukpbj
+	ON paket.ukpbj_id = ukpbj.ukpbj_id
 JOIN
-	PUBLIC. lelang_seleksi
-ON paket.pkt_id = lelang_seleksi.pkt_id
+	PUBLIC.lelang_seleksi
+	ON paket.pkt_id = lelang_seleksi.pkt_id
 JOIN
-	PUBLIC. panitia
-ON paket.pnt_id = panitia.pnt_id
-
+	PUBLIC.panitia
+	ON paket.pnt_id = panitia.pnt_id
 WHERE
 paket.pkt_nama LIKE '%Preservasi%' --OR --lelang_seleksi.lls_id = 74198064
 ORDER BY 1 DESC;
