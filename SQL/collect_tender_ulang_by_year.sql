67920d278e9163bcef3f4da6f3c9692e3b890e2d
Maulana Reza Pahlevi <79183291+mrezap@users.noreply.github.com>
2021-09-30 15:01:42 +0700

    Create collect_tender_ulang_by_year.sql

---
+++ SQL/collect_tender_ulang_by_year.sql
@@ -0,0 +1,35 @@
+SELECT
+	paket_sirup.tahun,
+	ukpbj.nama, 
+    lelang_seleksi.lls_id, 
+    paket.pkt_nama, 
+	paket.pkt_pagu,
+	kategori.keterangan,
+	--lelang_seleksi.lls_status,
+	--lelang_seleksi.lls_penawaran_ulang,
+	--lelang_seleksi.lls_evaluasi_ulang,
+	CASE
+		WHEN lelang_seleksi.lls_status = 2 THEN 'Tender Ulang'
+		ELSE ''
+	END AS status
+FROM
+	PUBLIC.lelang_seleksi
+JOIN
+	PUBLIC.paket
+	ON lelang_seleksi.pkt_id = paket.pkt_id
+JOIN
+	PUBLIC.ukpbj
+	ON paket.ukpbj_id = ukpbj.ukpbj_id
+JOIN
+	PUBLIC.kategori
+	ON paket.kgr_id = kategori.kgr_id
+JOIN
+	PUBLIC.paket_satker
+	ON paket.pkt_id = paket_satker.pkt_id
+JOIN
+	PUBLIC.paket_sirup
+	ON paket_satker.rup_id = paket_sirup.id
+WHERE
+paket_sirup.tahun = 2021 --you can adjust year in here as depends on paket year thats regist in SIRUP
+AND	lelang_seleksi.lls_status = 2 -- as information paket thats have value 2 is "tender ulang"
+ORDER BY 7 DESC, 2 ASC
